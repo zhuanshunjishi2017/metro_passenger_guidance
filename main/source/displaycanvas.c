c739319d4c792866ba441a2f923c8e51a25d18cb
zhaunshunjishi2017 <Zhuanshunjishi2017@outlook.com>
2026-04-08 13:44:29 +0800

    metro_draw

--- main/source/displaycanvas.c
+++ main/source/displaycanvas.c
@@ -2,28 +2,59 @@
 #include "lv_port_disp_template.h"
 #include "lv_port_indev_template.h"
 #include "lvgl.h"
-#define CANVAS_SIZE 4096    // 画布边长 4096x4096（可根据内存改 2048/8192）
-#define COLOR_FORMAT LV_IMG_CF_TRUE_COLOR  // 16位色，占用内存小
+#include <string.h>
 
-static lv_color_t canvas_buf[CANVAS_SIZE * CANVAS_SIZE];
 
-void display0_init(lv_obj_t * display0)
-{
-    lv_obj_set_style_border_width(display0, 0, LV_PART_MAIN);
-	lv_obj_set_style_bg_opa(display0, 0, LV_PART_MAIN);
-	lv_obj_set_scrollbar_mode(display0, LV_SCROLLBAR_MODE_OFF);
-	lv_obj_set_scroll_dir(display0, LV_DIR_ALL);
-	lv_obj_set_scroll_snap(display0, LV_SCROLL_SNAP_NONE);
-	//无边界滚动
-	lv_obj_set_scroll_limit(display0, LV_DIR_TOP, -LV_COORD_MAX);
-    lv_obj_set_scroll_limit(display0, LV_DIR_BOTTOM, LV_COORD_MAX);
-    lv_obj_set_scroll_limit(display0, LV_DIR_LEFT, -LV_COORD_MAX);
-    lv_obj_set_scroll_limit(display0, LV_DIR_RIGHT, LV_COORD_MAX);
-}
+#define CANVAS_X 60
+#define CANVAS_Y 55
+#define CANVAS_W 964
+#define CANVAS_H 545
+#define BUF_SIZE LV_CANVAS_BUF_SIZE_TRUE_COLOR(CANVAS_W,CANVAS_H)
+
+
+void *canvas_buf;//缓冲区
+void pressing_canvas(lv_event_t *e);
+void canvas_init(lv_obj_t *);
+
+extern lv_coord_t origin_x, origin_y;
 
 void canvas_init(lv_obj_t *canvas)
 {
-	lv_canvas_set_buffer(canvas, canvas_buf, CANVAS_SIZE, CANVAS_SIZE, COLOR_FORMAT);
-    lv_obj_center(canvas);
-	lv_canvas_fill_bg(canvas, lv_color_white(), LV_OPA_COVER);
+	    //申请缓冲区以设置像素
+    canvas_buf = sdram_malloc(BUF_SIZE);
+    if (!canvas_buf) while (1);
+    
+    lv_obj_set_pos(canvas,CANVAS_X,CANVAS_Y);
+    lv_obj_set_size(canvas,CANVAS_W,CANVAS_H);
+
+    lv_canvas_set_buffer(canvas, canvas_buf, CANVAS_W, CANVAS_H, LV_IMG_CF_TRUE_COLOR);
+
+    lv_obj_add_flag(canvas, LV_OBJ_FLAG_CLICKABLE);
+
+    lv_obj_add_event_cb(canvas, pressing_canvas, LV_EVENT_PRESSING, NULL);
+    lv_canvas_fill_bg(canvas, lv_color_white(), LV_OPA_COVER);
+
+    create_metro_map(canvas);
+}
+
+void pressing_canvas(lv_event_t *e)
+{
+    lv_obj_t * obj = lv_event_get_target(e);//获取产生这个事件的对象
+    lv_indev_t * indev = lv_indev_get_act();//获取输入设备
+
+    lv_point_t vect;
+    lv_indev_get_vect(indev, &vect);
+/* 
+    lv_coord_t x = lv_obj_get_x(obj) + vect.x;
+    lv_coord_t y = lv_obj_get_y(obj) + vect.y;
+    lv_obj_set_pos(obj, x, y);
+ */
+    origin_x += vect.x;
+    origin_y += vect.y;
+
+    //通过直接访问内存将画布重新填充为白色
+    memset(canvas_buf, 255, BUF_SIZE);
+
+    create_metro_map(obj);
+
 }
--- main/source/main.c
+++ main/source/main.c
@@ -2,13 +2,18 @@
 #include "lv_port_disp_template.h"
 #include "lv_port_indev_template.h"
 
-void gpio_init(void);
-void display0_init(lv_obj_t *);
-void canvas_init(lv_obj_t *);
 
-lv_obj_t *label;
+
+void gpio_init(void);
+// void display0_init(lv_obj_t *);
+
+
+
+
 lv_obj_t* display0;
 lv_obj_t* canvas;
+
+
 
 int main(void)
 {
@@ -16,36 +21,23 @@ int main(void)
 	
 	rcu_periph_clock_enable(RCU_GPIOA);
 	gpio_init();
+
+    my_rtc_setup();
 	
 	lv_init();
 	lv_port_disp_init();
 	lv_port_indev_init();
 
-	display();
-    
     //display&canvas
     display0 = lv_obj_create(NULL);
-    display_init(display0);
+    //display_init(display0);
     canvas = lv_canvas_create(display0);
     canvas_init(canvas);
-        
-    //rtc_time
-    lv_obj_t * btn;
-    btn = lv_btn_create(display0);
-    lv_obj_set_size(btn,200,50);
-    lv_obj_center(btn);
 
-    label = lv_label_create(btn);
-    lv_label_set_text(label, "");
-    lv_obj_center(label); 
-    lv_label_set_text(label,"start to configure RTC time\n\r");
-
-    my_rtc_setup();
-    char time_buf[32];
+    lv_scr_load(display0);
+    
     while(1)
 	{
-        get_rtc_time_string(time_buf, sizeof(time_buf));
-        lv_label_set_text(label, time_buf); // 更新显示
         lv_timer_handler();
         delay_us(40000);//刷新率25Hz
 	}
@@ -57,3 +49,4 @@ void gpio_init(void)
 	gpio_output_options_set(GPIOA, GPIO_OTYPE_PP, GPIO_OSPEED_60MHZ, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
 	gpio_bit_reset(GPIOA, GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2 | GPIO_PIN_3);
 }
+
